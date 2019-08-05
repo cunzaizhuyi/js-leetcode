e1c62c6cebd3bacfbbc2806a288c8fb6c6a1b029
杨帅伟 <877824709@qq.com>
2019-08-06 07:45:03 +0800

    283

---
+++ arr/unSortedArr/283-moveZero.js
@@ -0,0 +1,26 @@
+//283-easy-移动0
+
+// 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+//
+// 示例:
+// 输入: [0,1,0,3,12]
+// 输出: [1,3,12,0,0]
+
+// 说明:
+// 必须在原数组上操作，不能拷贝额外的数组。
+// 尽量减少操作次数。
+
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    for(let i =nums.length; i >= 0; i--){
+        if (nums[i] === 0){
+            nums.splice(i, 1)
+            nums.push(0)
+        }
+    }
+    return nums
+};
