99ddad0f1045927cd057b516d27c30026da08b44
杨帅伟 <877824709@qq.com>
2019-08-25 20:33:06 +0800

    155 394 121

--- 栈/155-easy-最小栈.js
+++ 栈/155-easy-最小栈.js
@@ -1,4 +1,22 @@
+// 155 easy 最小栈
 
+// 设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+//
+// push(x) -- 将元素 x 推入栈中。
+// pop() -- 删除栈顶的元素。
+// top() -- 获取栈顶元素。
+// getMin() -- 检索栈中的最小元素。
+
+// 示例:
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin();   --> 返回 -3.
+// minStack.pop();
+// minStack.top();      --> 返回 0.
+// minStack.getMin();   --> 返回 -2.
+//
 
 
 /**
