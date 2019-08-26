a3b1e06c81813a213ace5efbdf1fce0c1cae22d7
杨帅伟 <877824709@qq.com>
2019-08-27 07:14:13 +0800

    709

---
+++ 字符串/709-easy-转换成小写字母.js
@@ -0,0 +1,26 @@
+// 709 easy 转换成小写字母
+
+
+// 实现函数 ToLowerCase()，该函数接收一个字符串参数 str，并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
+//
+// 示例 1：
+// 输入: "Hello"
+// 输出: "hello"
+
+// 示例 2：
+// 输入: "here"
+// 输出: "here"
+
+// 示例 3：
+// 输入: "LOVELY"
+// 输出: "lovely"
+
+
+
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function(str) {
+    return str.toLowerCase()
+};
