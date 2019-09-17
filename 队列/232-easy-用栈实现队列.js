ea254a5a2acdeb959083053dbbcfb0695e8ac191
杨帅伟 <877824709@qq.com>
2019-09-17 22:24:45 +0800

    232

---
+++ 队列/232-easy-用栈实现队列.js
@@ -0,0 +1,73 @@
+// 232 easy 用栈实现队列
+
+// 使用栈实现队列的下列操作：
+//
+// push(x) -- 将一个元素放入队列的尾部。
+// pop() -- 从队列首部移除元素。
+// peek() -- 返回队列首部的元素。
+// empty() -- 返回队列是否为空。
+// 示例:
+//
+//     MyQueue queue = new MyQueue();
+//
+// queue.push(1);
+// queue.push(2);
+// queue.peek();  // 返回 1
+// queue.pop();   // 返回 1
+// queue.empty(); // 返回 false
+// 说明:
+//
+//     你只能使用标准的栈操作 -- 也就是只有 push to top, peek/pop from top, size, 和 is empty 操作是合法的。
+// 你所使用的语言也许不支持栈。你可以使用 list 或者 deque（双端队列）来模拟一个栈，只要是标准的栈操作即可。
+// 假设所有操作都是有效的 （例如，一个空的队列不会调用 pop 或者 peek 操作）。
+
+
+
+/**
+ * Initialize your data structure here.
+ */
+var MyQueue = function() {
+    this.arr = []
+};
+
+/**
+ * Push element x to the back of queue.
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    this.arr.push(x)
+};
+
+/**
+ * Removes the element from in front of queue and returns that element.
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    return this.arr.shift()
+};
+
+/**
+ * Get the front element.
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.arr[0]
+};
+
+/**
+ * Returns whether the queue is empty.
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.arr.length === 0
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
