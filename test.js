09b5a7969ad839d103d24ce2b283b9c651fbda08
杨帅伟 <877824709@qq.com>
2019-10-01 18:50:51 +0800

    986

---
+++ test.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var insertionSortList = function(head) {
+    if(!head)return head;
+    let cNode = head;
+    while(cNode){
+
+    }
+};
