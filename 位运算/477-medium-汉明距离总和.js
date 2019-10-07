f0d43abcde73a3664ddb80da39ed7e817ed05003
杨帅伟 <877824709@qq.com>
2019-10-07 11:49:30 +0800

    477

--- 位运算/477-medium-汉明距离总和.js
+++ 位运算/477-medium-汉明距离总和.js
@@ -1,29 +1,18 @@
-// 参考460 easy 汉明距离
+// 477 medium 汉明距离总和
 
-
-var hammingDistance = function(x, y) {
-    if(x === y) return 0;
-    let bin1 = x.toString(2)
-    let bin2 = y.toString(2)
-    let lenDif = Math.abs(bin2.length - bin1.length);
-    let zeroStr = '0'.repeat(lenDif);
-    let minLong
-    let maxNumberStr
-    if (x > y) {
-        minLong = zeroStr + bin2
-        maxNumberStr = bin1
-    } else {
-        minLong = zeroStr + bin1
-        maxNumberStr = bin2
-    }
-    let count = 0;
-    for(let i = 0; i < minLong.length; i++){
-        if(minLong[i] !== maxNumberStr[i]){
-            count++
-        }
-    }
-    return count;
-};
+// 两个整数的 汉明距离 指的是这两个数字的二进制数对应位不同的数量。
+// 计算一个数组中，任意两个数之间汉明距离的总和。
+//
+// 示例:
+// 输入: 4, 14, 2
+// 输出: 6
+//
+// 解释: 在二进制表示中，4表示为0100，14表示为1110，2表示为0010。（这样表示是为了体现后四位之间关系）
+// 所以答案为：
+// HammingDistance(4, 14) + HammingDistance(4, 2) + HammingDistance(14, 2) = 2 + 2 + 2 = 6.
+// 注意:
+// 数组中元素的范围为从 0到 10^9。
+// 数组的长度不超过 10^4。
 
 
 /**
@@ -31,13 +20,20 @@ var hammingDistance = function(x, y) {
  * @return {number}
  */
 var totalHammingDistance = function(nums) {
+    if(!nums.length) return 0;
+
     let total = 0;
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            total += hammingDistance(nums[i], nums[j])
+    // 不超过32位
+    for(let i = 0; i < 32; i++){
+        let oneCount = 0 // 1的个数
+        for(let j = 0; j < nums.length; j++){
+            oneCount += nums[j] & 1; // 看看是不是1， 是就累加上
+            nums[j] = nums[j] >> 1; // 除2
         }
+        total += oneCount * (nums.length - oneCount) // 1的个数乘以0的个数
     }
     return total
 };
 
 console.log(totalHammingDistance([4,14,2]))
+
