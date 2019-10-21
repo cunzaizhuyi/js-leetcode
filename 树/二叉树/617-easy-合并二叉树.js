09508294d3f2e64ebe99ad0d9c2a94e11e2bbc49
杨帅伟 <877824709@qq.com>
2019-10-21 23:56:25 +0800

    617

--- 树/二叉树/617-easy-合并二叉树.js
+++ 树/二叉树/617-easy-合并二叉树.js
@@ -1,4 +1,9 @@
+// 617 easy 合并二叉树
 
+// 给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+// 你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，
+// 否则不为 NULL 的节点将直接作为新二叉树的节点。
+//
 
 /**
  * Definition for a binary tree node.
@@ -13,23 +18,41 @@
  * @return {TreeNode}
  */
 var mergeTrees = function(t1, t2) {
-    if(!t1)return t2;
-    if(!t2)return t1;
+    if(!t1) return t2;
+    if(!t2) return t1;
 
-    let cNode1 = t1;
-    let cNode2 = t2;
-    let tree = new TreeNode(t1.val + t2.val);
+    let fn = (l, r) => {
+        l.val = l.val + r.val;
 
-    let fn = (cNode1, cNode2, tree) => {
-        if(cNode1.left || cNode2.left){
-            tree.left = new TreeNode(~~cNode1.left.val + ~~cNode2.left.val);
+        if(!l.left && !r.left){
+            return;
         }
-        if(cNode1.right || cNode2.right){
-            tree.right = new TreeNode(~~cNode1.right.val + ~~cNode2.right.val);
+        if(!l.right && !r.right){
+            return;
         }
-        fn(cNode1.left, cNode1.left, tree.left);
-        fn(cNode1.right, cNode1.right, tree.right);
+        if(l.left && r.left){
+            fn(l.left, r.left)
+        }
+        if(l.right && r.right){
+            fn(l.right, r.right);
+        }
+        if(l.left && !r.left){
+            r.left = new TreeNode(0)
+            fn(l.left, r.left)
+        }
+        if(!l.left && r.left){
+            l.left = new TreeNode(0)
+            fn(l.left, r.left)
+        }
+        if(l.right && !r.right){
+            r.right = new TreeNode(0)
+            fn(l.right, r.right)
+        }
+        if(!l.right && r.right){
+            l.right = new TreeNode(0)
+            fn(l.right, r.right)
+        }
     }
-    fn(cNode1, cNode2, tree)
-    return tree;
+    fn(t1, t2)
+    return t1
 };
