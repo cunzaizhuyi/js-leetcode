4b498cf2ca8f42395799f35db070e7943e2f619f
cunzaizhuyi <877824709@qq.com>
2020-01-06 22:01:48 +0800

    1302

---
+++ 树/二叉树/1302-medium-层数最深叶子节点的和.js
@@ -0,0 +1,32 @@
+//1302. 层数最深叶子节点的和 medium
+
+//给你一棵二叉树，请你返回层数最深的叶子节点的和。
+
+var deepestLeavesSum = (root) => {
+    if (!root) return 0;
+    let bfs = (nodes) => {
+        let result = [];
+        let hasSonNodes =  []
+        for (let i = 0; i < nodes.length; i++){
+            if (!nodes[i].left && !nodes[i].right){
+                result.push(nodes[i].val)
+            } else {
+                if (nodes[i].left){
+                    hasSonNodes.push(nodes[i].left);
+                }
+                if(nodes[i].right){
+                    hasSonNodes.push(nodes[i].right);
+                }
+            }
+        }
+        if (!hasSonNodes.length){
+            return result;
+        }
+        return bfs(hasSonNodes)
+    }
+    let result = bfs([root])
+
+    return result.reduce((p, n) => {
+        return p + n;
+    }, 0)
+}
