932205fec85aff009bcc35b5d66f257b24c436b8
cunzaizhuyi <877824709@qq.com>
2020-02-16 17:58:38 +0800

    03
    Signed-off-by: cunzaizhuyi <877824709@qq.com>

---
+++ 剑指offer/03-easy-数组中重复的数字.js
@@ -0,0 +1,26 @@
+// 03 easy 数组中重复的数字
+
+// 找出数组中重复的数字。
+//
+// 在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。
+// 数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请找出数组中任意一个重复的数字。
+// 示例 1：
+//
+// 输入：
+// [2, 3, 1, 0, 2, 5, 3]
+// 输出：2 或 3
+//  
+// 限制：
+// 2 <= n <= 100000
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findRepeatNumber = function(nums) {
+    let hash = {};
+    for(let i = 0; i < nums.length; i++){
+        if(hash[nums[i]]) return nums[i];
+        hash[nums[i]] = true;
+    }
+};
