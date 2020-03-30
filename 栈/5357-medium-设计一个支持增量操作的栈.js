0cb83f432e81dffbaac0d89119dc502f38018660
cunzaizhuyi <877824709@qq.com>
2020-03-30 09:19:18 +0800

    5357

--- 栈/5357-medium-设计一个支持增量操作的栈.js
+++ 栈/5357-medium-设计一个支持增量操作的栈.js
@@ -1,7 +1,14 @@
 // 设计一个支持增量操作的栈 5357 medium
 
-
-
+// 请你设计一个支持下述操作的栈。
+//
+// 实现自定义栈类 CustomStack ：
+//
+// CustomStack(int maxSize)：用 maxSize 初始化对象，maxSize 是栈中最多能容纳的元素数量，栈在增长到 maxSize 之后则不支持 push 操作。
+// void push(int x)：如果栈还未增长到 maxSize ，就将 x 添加到栈顶。
+// int pop()：返回栈顶的值，或栈为空时返回 -1 。
+// void inc(int k, int val)：栈底的 k 个元素的值都增加 val 。如果栈中元素总数小于 k ，则栈中的所有元素都增加 val 。
+ 
 
 /**
  * @param {number} maxSize
