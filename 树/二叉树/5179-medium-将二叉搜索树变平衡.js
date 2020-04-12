104c7ea78761f27c7a6b0007c84793219d2dea10
cunzaizhuyi <877824709@qq.com>
2020-04-12 08:29:42 +0800

    5179

--- 树/二叉树/5179-medium-将二叉搜索树变平衡.js
+++ 树/二叉树/5179-medium-将二叉搜索树变平衡.js
@@ -1,4 +1,4 @@
-// 5179 medium 
+// 5179 medium 将二叉搜索树变平衡
 
 
 // 给你一棵二叉搜索树，请你返回一棵 平衡后 的二叉搜索树，新生成的树应该与原来的树有着相同的节点值。
@@ -6,55 +6,39 @@
 // 如果有多种构造方法，请你返回任意一种。
 
 var balanceBST = function(root) {
-    let arr = []
+    // 获取节点值。bst中序遍历返回升序数组。
+    let arr = [];
     let fn = (node) => {
-        if(!node.left && !node.right){
-            arr.push(node.val);
-            return;
-        }
-        if(node.left){
-            fn(node.left)
-        }
-        arr.push(node.val)
-        if(node.right){
-            fn(node.right);
-        }
-    }
+        if(!node)return;
+        fn(node.left);
+        arr.push(node.val);
+        fn(node.right);
+    };
     fn(root);
-    // console.log(arr)
+
+    // 重建平衡bst
     let mid = ~~(arr.length / 2);
     let h = new TreeNode(arr[mid]);
-    let c = 1;
-    let ff = (node, arr, mid) => {
-        console.log(node, arr)
-        if(!arr.length || arr.length===1){
+    let buildTree = (node, arr, mid) => {
+        if(!arr.length || arr.length === 1){
             return;
         }
-        if(arr.length === 2){
-            if(node.val===arr[0]){
-                node.right = new TreeNode(arr[1])
-            }else{
-                node.left = new TreeNode(arr[0])
-            }
-            return
-        }
-        if(arr.length === 3){
-            node.left = new TreeNode(arr[0])
-            node.right = new TreeNode(arr[2])
+        if(arr.length === 2){ // 跟mid的划分有关，有两个数，mid是右侧那个
+            node.left = new TreeNode(arr[0]);
             return
         }
 
-        if(mid>=0){
-            node.left = new TreeNode(arr[~~(mid / 2)]);
-            ff(node.left, arr.slice(0, mid), ~~(mid / 2))
-        }
-        if(mid<=arr.length-1){
-            let a = arr.slice(mid+1);
-            let mi = ~~(a.length / 2);
-            node.right = new TreeNode(a[mi]);
-            ff(node.right, a, mi)
-        }
-    }
-    ff(h, arr, mid)
+        // 左树
+        node.left = new TreeNode(arr[~~(mid / 2)]);
+        buildTree(node.left, arr.slice(0, mid), ~~(mid / 2));
+
+        // 右树
+        let a = arr.slice(mid+1);
+        let mi = ~~(a.length / 2);
+        node.right = new TreeNode(a[mi]);
+        buildTree(node.right, a, mi)
+    };
+    // 参数解释：给定一个数组，已经锁定了head和head的index
+    buildTree(h, arr, mid);
     return h;
 };
