b38f525527f8044bec3f0bcb49cbe4d625f9f807
cunzaizhuyi <877824709@qq.com>
2020-07-11 12:08:20 +0800

    5452

---
+++ 数学类/5452-easy-判断能否形成等差数列.js
@@ -0,0 +1,36 @@
+
+//5452. 判断能否形成等差数列 easy
+
+// 给你一个数字数组 arr 。
+// 如果一个数列中，任意相邻两项的差总等于同一个常数，那么这个数列就称为 等差数列 。
+// 如果可以重新排列数组形成等差数列，请返回 true ；否则，返回 false 。
+//
+//  
+// 示例 1：
+// 输入：arr = [3,5,1]
+// 输出：true
+// 解释：对数组重新排序得到 [1,3,5] 或者 [5,3,1] ，任意相邻两项的差分别为 2 或 -2 ，可以形成等差数列。
+// 示例 2：
+// 输入：arr = [1,2,4]
+// 输出：false
+// 解释：无法通过重新排序得到等差数列。
+//  
+// 提示：
+// 2 <= arr.length <= 1000
+// -10^6 <= arr[i] <= 10^6
+
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function(arr) {
+    arr.sort((a,b)=>{return a-b});
+    let dif = arr[1] - arr[0]
+    for(let i= 2;i<arr.length;i++){
+        if(arr[i] - arr[i-1] !== dif){
+            return false;
+        }
+    }
+    return true;
+};
